f23408c72bf5069d75f4784267bc08807bda3d0f
stvansolano <stvansolano@gmail.com>
2015-03-22 14:18:01 -0600

    Scaffolded project using Xamarin Studio

---
+++ 01_HelloWorldCrossPlatform/CrossPlatformApp/CrossPlatformApp.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace CrossPlatformApp
+{
+	public class App : Application
+	{
+		public App ()
+		{
+			// The root page of your application
+			MainPage = new ContentPage {
+				Content = new StackLayout {
+					VerticalOptions = LayoutOptions.Center,
+					Children = {
+						new Label {
+							XAlign = TextAlignment.Center,
+							Text = "Hello World, from Xamarin Forms!"
+						}
+					}
+				}
+			};
+		}
+
+		protected override void OnStart ()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
+
