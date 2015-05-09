ed4777431e18937bf827b0aa10946e3585f326b3
stvansolano <stvansolano@gmail.com>
2015-05-09 10:46:46 -0600

    Added ToDoList

---
+++ FeaturedApps/ToDoApp/CrossPlatformApp/CrossPlatformApp/Views/ToDoList.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace CrossPlatformApp.Views
+{
+	public partial class ToDoList : ContentPage
+	{
+		public ToDoList()
+		{
+			InitializeComponent();
+		}
+	}
+}
