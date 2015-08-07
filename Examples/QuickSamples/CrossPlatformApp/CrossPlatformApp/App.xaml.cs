01ef28a40cf141ec418d39dc808bc886cffb575d
stvansolano <stvansolano@gmail.com>
2015-08-06 18:49:47 -0600

    Added QuickSamples project

---
+++ Examples/QuickSamples/CrossPlatformApp/CrossPlatformApp/App.xaml.cs
@@ -0,0 +1,36 @@
+﻿namespace CrossPlatformApp
+{
+    using Xamarin.Forms;
+
+    public partial class App
+    {
+        public App()
+        {
+            try
+            {
+                InitializeComponent();
+            }
+            catch (System.Exception ex)
+            {
+            }
+
+            // The root page of your application
+            MainPage = new DictionaryBindingExample();
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ Examples/QuickSamples/CrossPlatformApp/CrossPlatformApp/Pages/DictionaryBinding.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace CrossPlatformApp
+{
+    using System;
+    using System.Collections.Generic;
+
+    public partial class DictionaryBindingExample
+    {
+        public DictionaryBindingExample()
+		{
+            try
+            {
+                InitializeComponent();
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            var list = new Dictionary<string, string>() { 
+                { "a", "1" },
+                { "b", "2" },
+                { "c", "3" },
+                { "d", "4" },
+                { "e", "5" },
+                { "f", "6" },
+            };
+            listView.ItemsSource = list;
+		}
+    }
+}
