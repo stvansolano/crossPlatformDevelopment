3599c458b9a5a924bed6e5522c658f9a27cce644
stvansolano <stvansolano@gmail.com>
2015-08-17 13:29:24 -0700

    Fixed UITests

--- FeaturedApps/ToDoApp/UITests/AndroidTests.cs
+++ FeaturedApps/ToDoApp/UITests/AndroidTests.cs
@@ -10,31 +10,10 @@
     [TestFixture()]
     public class AndroidTest : CrossPlatformTests
     {
-        public string PathToAPK { get; set; }
-
-
-        [TestFixtureSetUp]
-        public void TestFixtureSetup()
-        {
-            string currentFile = new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath;
-            FileInfo fi = new FileInfo(currentFile);
-            string dir = fi.Directory.Parent.Parent.Parent.FullName;
-
-#if DEBUG
-            PathToAPK = Path.Combine(dir, "CrossPlatformApp", "CrossPlatformApp.Droid", "bin", "Debug", "com.stvansolano.todoapp.apk");
-#endif
-        }
-
         [SetUp]
         public override void SetUp()
         {
-            // an API key is required to publish on Xamarin Test Cloud for remote, multi-device testing
-            // this works fine for local simulator testing though
-            //App = ConfigureApp.Android.ApkFile(PathToAPK).ApiKey("36d0f463f0931d440289115b13087f27").StartApp();
-
-            //App = AppInitializer.StartApp(Platform.Android);
-
-            App = ConfigureApp.Android.ApiKey("2ad3fdc4d9dac33c02c44bb72858bd90").EnableLocalScreenshots().StartApp();
+            App = ConfigureApp.Android.StartApp();
 
             App.Screenshot("Given the app is loaded");
         }
